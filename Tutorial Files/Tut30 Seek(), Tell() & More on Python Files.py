4fd2d85e52f02463aa2cbb151e6791448fffd4b3
Harshil <harshilsharma.2020@gmail.com>
2023-08-03 22:56:06 +0530

    Minor Changes

--- Tutorial Files/Tut30 Seek(), Tell() & More on Python Files.py
+++ Tutorial Files/Tut30 Seek(), Tell() & More on Python Files.py
@@ -3,7 +3,7 @@ f = open("harshil2.txt")
 print(f.tell())
 print(f.readline())
 # print(f.tell())
-f.seek(0)                 #seek is matlab reset kr diya
+f.seek(0)                 #seek matlab reset kr diya
 print(f.readline())
 # print(f.tell())
 
