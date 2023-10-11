474bd8418a876da1bca1952d5bb8ed7906d86057
Harshil <harshilsharma.2020@gmail.com>
2023-10-11 21:52:38 +0530

    Q02: Made a few changes

--- DMW Lab Sem-V/Q02_FibonacciSeries.py
+++ DMW Lab Sem-V/Q02_FibonacciSeries.py
@@ -1,19 +1,20 @@
-def fibonacci(n):
+def fibonacci(num):
     n1, n2, count = 0, 1, 0
-    if n <= 0:
+    if num <= 0:
         return "Invalid Entry!"
-    if n == 1:
+    if num == 1:
         return n1
-    if n == 2:
+    if num == 2:
         return n2
     else:
-        while count < n:
+        while count < num:
             print(n1)
-            nNew = n1 + n2
-            #updating value
+            new = n1 + n2
+            # updating value
             n1 = n2
-            n2 = nNew
+            n2 = new
             count += 1
+
 
 n = int(input("Enter the number of terms for Fibonacci Series: "))
 print("The Fibonacci Sequence for", n, "number of terms:")
