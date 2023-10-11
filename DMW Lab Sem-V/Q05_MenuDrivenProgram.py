ca829a09106be29853cba642ef80d963dde9eaf0
Harshil <harshilsharma.2020@gmail.com>
2023-10-11 22:25:35 +0530

    Q05: Made a few changes

--- DMW Lab Sem-V/Q05_MenuDrivenProgram.py
+++ DMW Lab Sem-V/Q05_MenuDrivenProgram.py
@@ -1,16 +1,20 @@
 def add(x, y):
     return x + y
 
+
 def subtract(x, y):
     return x - y
 
+
 def multiply(x, y):
     return x * y
+
 
 def divide(x, y):
     if y == 0:
         return "Cannot divide by zero."
     return x / y
+
 
 print("Calculator!")
 x = int(input("Enter number 1: "))
@@ -18,13 +22,13 @@ y = int(input("Enter number 2: "))
 print("\t1 for Addition\n\t2 for Subtraction\n\t3 for Multiplication\n\t4 for Division")
 n = int(input("Enter your input: "))
 
-if (n == 1):
-    print(x,"+",y,"=",x+y)
-elif (n == 2):
-    print(x,"-",y,"=",x-y)
-elif (n == 3):
-    print(x,"*",y,"=",x*y)
-elif (n == 4):
-    print(x,"/",y,"=",x/y)
+if n == 1:
+    print(x, "+", y, "=", x + y)
+elif n == 2:
+    print(x, "-", y, "=", x - y)
+elif n == 3:
+    print(x, "*", y, "=", x * y)
+elif n == 4:
+    print(x, "/", y, "=", x / y)
 else:
     print("Invalid Entry!")
