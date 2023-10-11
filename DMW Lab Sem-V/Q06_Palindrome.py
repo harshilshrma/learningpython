3dd6e44ec818fc8b9e42fb167bb509c133bb977b
Harshil <harshilsharma.2020@gmail.com>
2023-10-11 22:25:42 +0530

    Q06: Made a few changes

--- DMW Lab Sem-V/Q06_Palindrome.py
+++ DMW Lab Sem-V/Q06_Palindrome.py
@@ -1,13 +1,14 @@
-def checkPalindrome(n):
-    originalNum, reversedNum, rem = n, 0, 0
-    while (n > 0):
-        rem = n % 10
-        reversedNum = reversedNum*10 + rem
-        n = n // 10
+def checkPalindrome(num):
+    originalNum, reversedNum, rem = num, 0, 0
+    while num > 0:
+        rem = num % 10
+        reversedNum = reversedNum * 10 + rem
+        num = num // 10
     if reversedNum == originalNum:
         return True
     else:
         return False
+
 
 n = int(input("Enter a number to check for palindrome: "))
 print(checkPalindrome(n))
